0e919b1b57e9e8da148e56227d5ec4bf8038610e
Jose Daniel Hernandez <josedanielhq@gmail.com>
2020-06-26 11:14:20 -0500

    PWA aplicado

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifestPlugin = require("webpack-pwa-manifest");
+const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   output: {
@@ -8,6 +11,42 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
+    }),
+    new WebpackPwaManifestPlugin({
+      name: "Petgram - App de fotos mascotas",
+      shortname: "Petgram",
+      description:
+        "En Petgram puedes encontrar fotos de tus animales favoritos",
+      background_color: "#fff",
+      theme_color: "#b1a",
+      icons: [
+        {
+          src: path.resolve("src/assets/icon.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+        },
+      ],
+    }),
+    new WorkboxWebpackPlugin.GenerateSW({
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp(
+            "https://(res.cloudinary.com|images.unsplash.com)"
+          ),
+          handler: "CacheFirst",
+          options: {
+            cacheName: "images",
+          },
+        },
+        {
+          urlPattern: new RegExp(
+            "https://petgram-server-jose-daniel-25ci2soii.vercel.app"
+          ),
+          handler: "NetworkFirst",
+          options: {
+            cacheName: "api",
+          },
+        },
+      ],
     }),
   ],
   module: {
